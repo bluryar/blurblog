bfdbf53c0802c108520cef0150fcb5d3c8697534
bluryar <ljkblury@outlook.com>
2019-10-18 21:43:13 +0800

    fix: 修改几处变量名错误

--- server/config/config.dev.js
+++ server/config/config.dev.js
@@ -15,12 +15,13 @@ module.exports = {
       user: '',
       pass: '',
       dbName: 'blurblog@DEV',
-      poolize: 5, // mongodb默认为5
-      promiseLibary: Promise, // Promise库设置为ES6的Promise
+      poolSize: 5, // mongodb默认为5
+      promiseLibrary: Promise, // Promise库设置为ES6的Promise
       bufferCommands: false, // 要求mongoose 不缓存命令
       bufferMaxEntries: 0,
       useCreateIndex: true, // use createIndex() instead of ensureIndex()
       useNewUrlParser: true, // 使用新的解析器必须在连接字符串中指定端口
+      useUnifiedTopology: true,
       connectTimeoutMS: 5000 // 最大空闲连接时间，如果在这个时间内没有活动就关闭连接
     }
   }
