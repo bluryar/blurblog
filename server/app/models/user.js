7d8a8a868398e7dfed07d4e3c06ca4db0fc3fc15
bluryar <ljkblury@outlook.com>
2019-10-18 21:57:29 +0800

    improvement: 完成User模型

---
+++ server/app/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const options = {
+  // 严格模式
+  strict: true,
+  strictQuery: true,
+  useNestedStrict: true,
+  // 时间戳
+  timestamp: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+}
+const UserSchema = new mongoose.Schema({
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  isViolated: { // 是否违规用户
+    type: Boolean,
+    default: false
+  },
+  // id, mongodb自动创建并设置索引
+  nickname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  deleted_at: {
+    type: Date
+  }
+}, options)
+
+module.exports = mongoose.model('Users', UserSchema)
