6d8f49ca933b2bc5644e9d936ea96484c96091d0
bluryar <ljkblury@outlook.com>
2019-10-18 21:42:37 +0800

    fix: 修改AppManager，与App实例解耦
    BREAKING CHANGE: am实例提供一个load()方法，进行设置全局变量（config\HttpError\logger）

--- server/app.js
+++ server/app.js
@@ -1,21 +1,28 @@
-const AppManager = require('./lib/AppManager')
 
-const httpErrorCatcher = require('./app/middleware/httpErrorCatcher')// 引入全局http异常处理中间件
+/// 自定义中间件
+const httpErrorCatcher = require('./app/middleware/httpErrorCatcher') // 引入全局http异常处理中间件
 const httpLogger = require('./app/middleware/httpLogger') // http日志便捷中间件
+const rootRouter = require('./app/routes/index')
 
+/// 第三方中间件
 const Koa = require('koa')
 const cors = require('@koa/cors')
 const bodypaser = require('koa-bodyparser')
+
+// Koa实例
 const app = new Koa()
-const am = new AppManager(app) // 获取管理者单例，并且加载全局配置、日志、异常
+// AppManager实例
+const am = require('./lib/AppManager')(app) // 获取管理者单例
+
+am.load() // 设置全局变量 config\logger\HttpError
 
 app
   .use(cors()) // 处理跨域问题
   .use(httpLogger()) // 为ctx添加logger
   .use(httpErrorCatcher) // 全局异常处理
   .use(bodypaser()) // 参数解析
-
-am.loadRouterFileToApp(app) // 添加路由
+  .use(rootRouter.routes()) // 添加路由
+  .use(rootRouter.allowedMethods())
 
 app.listen(global.config.port, () => {
   console.log(`Your application is running at http://localhost:${global.config.port}`)
--- server/lib/AppManager.js
+++ server/lib/AppManager.js
@@ -1,74 +1,73 @@
 const EventEmitter = require('events')
 
-const configDefault = require('../config/config.default')
-const configDev = require('../config/config.dev')
-const configProd = require('../config/config.prod')
-const configTest = require('../config/config.test')
-
-const HttpError = require('./http-error')
-const getLog4js = require('./logger')
 /**
  * 加载配置到全局环境、并且对不同配置根据优先级进行覆盖操作
  */
 const loadConfigToGlobal = (function () {
   if (!process.env.NODE_ENV) throw new Error('未设置应用环境(NODE_ENV=undefined)')
   const tempConfig = {
-    default: configDefault,
-    dev: configDev,
-    prod: configProd,
-    test: configTest
+    default: require('../config/config.default'),
+    dev: require('../config/config.dev'),
+    prod: require('../config/config.prod'),
+    test: require('../config/config.test')
   }
   if (!tempConfig[process.env.NODE_ENV]) throw new Error(`环境设置错误(NODE_ENV=${process.env.NODE_ENV}`)
-  const config = {
-    ...configDefault, // default的优先级最低
-    ...tempConfig[process.env.NODE_ENV]
-  }
+
   return async function () {
-    global.config = config
+    global.config = {
+      ...tempConfig.default, // default的优先级最低
+      ...tempConfig[process.env.NODE_ENV]
+    }
   }
 })()
 
 const loadHttpErrorToGlobal = async () => {
-  global.HttpError = HttpError
+  global.HttpError = require('./http-error')
 }
 
 const loadLoggerToGlobal = async () => {
-  const log4js = getLog4js()
-  global.log = {
+  const log4js = require('./logger')()
+  global.logger = {
     HTTP_ERROR: log4js.getLogger('HTTP_ERROR'),
     CUSTOM_ERROR: log4js.getLogger('CUSTOM_ERROR'),
     DATABASE: log4js.getLogger('DATABASE'),
-    /**
-     * @property 代替console.log
-     */
     default: log4js.getLogger('default')
   }
 }
 
 /**
- * @todo 路由添加
  * @event configMounted 已经添加全局配置
  * @event errMounted 已经添加全局异常
  * @event loggerMounted 已经添加全局日志对象
- * @event routerMounted 已经讲路由挂载到App实例下
+ * @event success 所有全局配置以及设置成功
  */
 class AppManager extends EventEmitter {
   constructor (app) {
     super()
     // 创建单例
     if (!AppManager.instance) {
-      this.app = app
-      loadConfigToGlobal().then(() => this.emit('configMounted')) // 利用箭头函数没有绑定this
-      loadHttpErrorToGlobal().then(() => this.emit('errorMounted'))
-      loadLoggerToGlobal().then(() => this.emit('logerMounted'))
+      this._app = app
+      AppManager.instance = this
       return this
     } else {
       return AppManager.instance
     }
   }
 
+  load () {
+    loadConfigToGlobal()
+    loadHttpErrorToGlobal()
+    loadLoggerToGlobal()
+  }
+
+  /**
+   * @deprecated 将自动加载模块移动到routes目录下的index.js
+   */
   loadRouterFileToApp () {
-    this.emit('routerMounted', this.app)
+    // this.emit('routerMounted', this.app)
+    throw new Error('遗留接口，已废弃')
   }
 }
-module.exports = AppManager
+module.exports = function (app) {
+  return new AppManager(app)
+}
