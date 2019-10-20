859c936dff3af4bafa88e2f82b9c71a1a4754112
bluryar <ljkblury@outlook.com>
2019-10-20 10:10:33 +0800

    improvement: 显式地声明如果校验不通过时发生的逻辑

--- server/app/routes/user/admin.js
+++ server/app/routes/user/admin.js
@@ -6,9 +6,13 @@ const adminRouter = new KoaRouter({
 
 adminRouter.post('/register', async (ctx) => {
   if (!ctx.is('application/json')) throw new global.HttpError(global.HttpError.UnsupportedMediaType)
-  // 校验参数
+  // 生成校验器
   const validator = new ctx.validators.AdminValidator(ctx.request.body)
-  validator.checkPayload('register') // 拦截器，如果输入参数不符合将会抛出错误
+  // 拦截器，如果输入参数不符合将会抛出错误，从而被外层中间件捕获
+  if (validator.checkPayload('register') === true) {
+    ctx.logger(ctx.ip)
+    ctx.throw(500) // 如果该语句触发说明程序发生了严重且隐蔽的异常
+  }
 
   ctx.body = { msg: '注册成功', code: 200, errorCode: 0 }
   ctx.status = 200
