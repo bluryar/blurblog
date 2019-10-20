e451c76d4a2364a69db856a6931f845e20331c0e
bluryar <ljkblury@outlook.com>
2019-10-20 10:30:43 +0800

    fix: 校验逻辑

--- server/app/routes/user/admin.js
+++ server/app/routes/user/admin.js
@@ -10,7 +10,7 @@ adminRouter.post('/register', async (ctx) => {
   const validator = new ctx.validators.AdminValidator(ctx.request.body)
   // 拦截器，如果输入参数不符合将会抛出错误，从而被外层中间件捕获
   if (validator.checkPayload('register') === true) {
-    ctx.logger(ctx.ip)
+    ctx.logger.warn(`user ip:  ${ctx.ip}`)
   } else ctx.throw(500) // 如果该语句触发说明程序发生了严重且隐蔽的异常
 
   ctx.body = { msg: '注册成功', code: 200, errorCode: 0 }
