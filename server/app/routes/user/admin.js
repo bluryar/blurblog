20c06598e23b6a1c161718046330c1274f6c65c6
bluryar <ljkblury@outlook.com>
2019-10-20 18:08:56 +0800

    improvement: 初步完成Admin Register接口的逻辑且通过测试

--- server/app/routes/user/admin.js
+++ server/app/routes/user/admin.js
@@ -4,16 +4,31 @@ const adminRouter = new KoaRouter({
   prefix: '/user/admin'
 })
 
+adminRouter.use(async (ctx, next) => {
+  // 传入数据库连接实例，获取 AdminDao静态类,将这个静态类挂载到ctx下
+  ctx.AdminDao = require('../../../lib/dao/user/AdminDao')(ctx.db)
+  await next()
+})
+
 adminRouter.post('/register', async (ctx) => {
   if (!ctx.is('application/json')) throw new global.HttpError(global.HttpError.UnsupportedMediaType)
+
   // 生成校验器
   const validator = new ctx.validators.AdminValidator(ctx.request.body)
+
   // 拦截器，如果输入参数不符合将会抛出错误，从而被外层中间件捕获
   if (validator.checkPayload('register') === true) {
-    ctx.logger.warn(`user ip:  ${ctx.ip}`)
+    global.logger.default.info(`user ip:  ${ctx.ip}`)
   } else ctx.throw(500) // 如果该语句触发说明程序发生了严重且隐蔽的异常
 
-  ctx.body = { msg: '注册成功', code: 200, errorCode: 0 }
+  // 创建管理员
+  await ctx.AdminDao.createAdmin(ctx.request.body)
+
+  ctx.body = {
+    msg: '注册成功',
+    code: 200,
+    custom_code: 0
+  }
   ctx.status = 200
 })
 adminRouter.post('/login', async (ctx) => {})
