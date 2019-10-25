8ff7fe35e96a2b63e3667dd144a34a75c3c398ca
bluryar <ljkblury@outlook.com>
2019-10-25 16:07:56 +0800

    test: 添加Article的校验器测试

---
+++ server/test/lib/validator/ArticleValidator.test.js
@@ -0,0 +1,71 @@
+/* eslint-disable */
+require('../../app')
+
+const {
+  expect
+} = require('chai')
+const ArticleValidator = require('../../../lib/validators/ArticleValidator')
+describe('ArticleValidator', () => {
+  it('#checkPayload 空对象', () => {
+    try {
+      const validator = new ArticleValidator({})
+      validator.checkPayload()
+    } catch (error) {
+      expect(error.code).to.be.equal(400)
+    }
+  })
+  it('#checkPayload 对象属性为空', () => {
+    try {
+      const validator = new ArticleValidator({
+        title: undefined,
+        author: undefined,
+        content: undefined,
+        cover: undefined,
+        category_id: undefined
+      })
+      validator.checkPayload()
+    } catch (error) {
+      console.log(error)
+      expect(error.code).to.be.equal(400)
+    }
+  })
+  it('#checkPayload 对象属性为空字符', () => {
+    try {
+      const validator = new ArticleValidator({
+        title: "",
+        author: "",
+        content: "",
+        cover: ""
+      })
+      validator.checkPayload()
+    } catch (error) {
+      expect(error.code).to.be.equal(422)
+    }
+  })
+  it('#checkPayload 通过检测', () => {
+    const validator = new ArticleValidator({
+      title: "123",
+      author: "123",
+      content: "123",
+      cover: "123",
+      category_id: "5dadadad14c823579457b746"
+    })
+    expect(validator.checkPayload()).to.be.true
+
+  })
+  it('#checkPayload 缺少分类id值', () => {
+    try {
+      const validator = new ArticleValidator({
+        title: "123",
+        author: "123",
+        content: "123",
+        cover: "123",
+      })
+      expect(validator.checkPayload()).to.be.true
+    } catch (error) {
+      console.log(error)
+      expect(error.code).to.be.equal(400)
+    }
+
+  })
+})
